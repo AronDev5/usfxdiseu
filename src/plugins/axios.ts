3e3c3d963e67b0d0a2a1b68ab56100aff742f592
AronDev5 <116810456+AronDev5@users.noreply.github.com>
2023-05-05 23:12:41 -0400

    Update axios.ts

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -10,7 +10,6 @@ axios.interceptors.request.use(config => {
   if (config.headers) {
     config.headers["Content-type"] = "application/json";
     config.headers["Authorization"] = "Bearer " + authStore.token;
-    config.headers["Access-Control-Allow-Origin"] = "*";
     config.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS";
     config.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization";
   }
